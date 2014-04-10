a62b8cbb33149b9e9eb5087146e80caabf39d7be
ForrestWidmer <forrest.widmer@gmail.com>
2014-04-10 15:57:54 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EstateNation::Application.config.secret_token = '9d7da1eb61f91ca9d325cb8c8d427c0badd75bc0f0b93a4fc2f90c4226ac94ba4ed82872139b4bb0b95d8ce429b52bfadf44b0de4c467f42276d083ad91150d0'
